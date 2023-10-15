8b40efad0a406a1843ea7e7f05883d876b58ed9c
josuebmota <josuebatistam1@gmail.com>
2023-10-15 15:56:15 -0300

    chore: finalizando sidebar

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,21 @@
+import { Post } from './Post.jsx'
+import { Header } from './components/Header.jsx'
+import { SideBar } from './components/Sidebar.jsx'
+
+import styles from './App.module.css'
+import './global.css'
+
+export function App() {
+  return (
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <SideBar/>
+        <Post 
+          author = "Josu" 
+          content = "asdhasdhasu" 
+          />
+      </div>
+    </div>
+  )
+}
