4838e6f321fb346729788bde9aa4bc8a9495ca19
josuebmota <josuebatistam1@gmail.com>
2023-10-15 21:50:07 -0300

    chore: atribuindo proprieadades ao post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -5,16 +5,41 @@ import { SideBar } from './components/Sidebar.jsx'
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+  {
+    id:1,
+    author: {
+      avatarUrl: 'https://github.com/josuebmota.png',
+      name: 'Josué',
+      role: 'Engenheiro de Software'
+    },
+    content: [
+      {type: 'paragraph', text: 'Fala glr'},
+      {type: 'paragraph', text: 'Fala glr'},
+      {type: 'paragraph', text: 'Fala glr'},
+      {type: 'link', text: '#learning reactjs'},
+    ],
+    publishedAt: new Date('2023-10-15 12:00:00'),
+  }
+]
+
 export function App() {
   return (
     <div>
       <Header/>
       <div className={styles.wrapper}>
         <SideBar/>
-        <Post 
-          author = "Josu" 
-          content = "asdhasdhasu" 
-          />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />)
+          })}          
+        </main>        
       </div>
     </div>
   )
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,36 +1,42 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { Avatar } from './Avatar.jsx'
 import { Comment } from './Comment.jsx'
 import styles from './Post.module.css'
 
-export function Post(props){
+export function Post({author, content, publishedAt}){
+  const publishedDateFormat = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {locale: ptBR})
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/josuebmota.png"/>
+          <Avatar src={author.avatarUrl}/>
 
           <div className={styles.authorInfo}>
-            <strong>Josue</strong>
-            <span>Web Developer</span>
-            <time>04 Jun 2021</time>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
         <time 
-          title='15 de outubro de 2023' 
-          dateTime="2023-10-13">
-            Publicado há 1h
+          title={publishedDateFormat}
+          dateTime={publishedAt.toISOString()}>
+            {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala glr</p>
-        <p> 
-          <a href="">#learning reactjs</a>{' '}
-          <a href="">#learning reactjs</a>{' '}
-          <a href="">#learning reactjs</a>{' '}
-          <a href="">#learning reactjs</a>{' '}
-        </p>
+        {content.map((content, index) => { 
+          if(content.type === 'paragraph'){
+            return <p key={index}>{content.text}</p>
+          }else if(content.type === 'link'){
+            return <p><a href="#">{content.text}</a></p>
+         }})}
       </div>
 
       <form className={styles.commentForm}>
